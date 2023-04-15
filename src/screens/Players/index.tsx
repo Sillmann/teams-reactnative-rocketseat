1014bd9f1f55224f3edb039bd3e827b8bf8c1819
ssilman <ssilman@gmail.com>
2023-04-15 09:40:06 -0300

    complements

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Filter } from '@components/Filter';
 
 export function Players(){
   return(
@@ -10,6 +13,25 @@ export function Players(){
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add"
+        />
+
+      </Form>
+
+      <Filter
+        title="Time A"
+        isActive
+      />
+      
     </Container>
   )
 }
